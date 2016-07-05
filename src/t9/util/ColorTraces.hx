087cb4c53e4df51f80f16cf819a93ea13fed6a8f
Dion Amago <dion@transition9.com>
2016-07-05 13:01:47 -0700

    More colors

--- src/t9/util/ColorTraces.hx
+++ src/t9/util/ColorTraces.hx
@@ -4,6 +4,10 @@ class ColorTraces
 {
 	inline static var RED="\033[0;31m";
 	inline static var GREEN="\033[0;32m";
+	inline static var YELLOW="\033[0;33m";
+	inline static var BLUE="\033[0;34m";
+	inline static var MAGENTA="\033[0;35m";
+	inline static var CYAN="\033[0;36m";
 	inline static var NC="\033[0m";//No Color
 
 	inline public static function traceRed(s :String)
@@ -14,5 +18,25 @@ class ColorTraces
 	inline public static function traceGreen(s :String)
 	{
 		trace('${GREEN}${s}${NC}');
+	}
+
+	inline public static function traceYellow(s :String)
+	{
+		trace('${YELLOW}${s}${NC}');
+	}
+
+	inline public static function traceBlue(s :String)
+	{
+		trace('${BLUE}${s}${NC}');
+	}
+
+	inline public static function traceMagenta(s :String)
+	{
+		trace('${MAGENTA}${s}${NC}');
+	}
+
+	inline public static function traceCyan(s :String)
+	{
+		trace('${CYAN}${s}${NC}');
 	}
 }
---
+++ test/Main.hx
@@ -0,0 +1,12 @@
+class Main
+{
+	static function main()
+	{
+		traceRed('This is red');
+		traceGreen('This is green');
+		traceYellow('This is yellow');
+		traceBlue('This is blue');
+		traceMagenta('This is magenta');
+		traceCyan('This is cyan');
+	}
+}
